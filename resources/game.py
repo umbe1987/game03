1a8f830d9646e0aee7afd3c3e11bef83f84a1141
Firstname Lastname <email_address>
2017-12-20 22:39:12 +0100

    improved map and character interaction

--- resources/game.py
+++ resources/game.py
@@ -10,14 +10,16 @@ def game():
     pygame.init()    
     fpsClock = pygame.time.Clock()
     pygame.display.set_caption(TITLE) # set the title in the caption
-        
+    
     DISPLAYSURF = pygame.display.set_mode(SCREEN_SIZE)
+    # draw Tiled map (.tmx) onto screen
+    tile_manager.render_tiles_to_screen(MAP,  DISPLAYSURF)
     hero = character.Hero(12) # instance of Hero character
     hero_group = pygame.sprite.Group()
     hero_group.add(hero)
     
     hero_group.draw(DISPLAYSURF) # draw hero_group onto display Surface
-    pygame.display.update(hero.rect) # Update portions (rect or rect list) of the screen
+    #pygame.display.update(hero.rect) # Update portions (rect or rect list) of the screen
     
     while True: # main game loop
         for event in pygame.event.get():
@@ -29,7 +31,6 @@ def game():
         hero.move()
         # pygame.event.pump() # internally process pygame event handlers
         hero_group.draw(DISPLAYSURF) # draw hero_group onto display Surface
-        tile_manager.render_tiles_to_screen(MAP,  DISPLAYSURF)
         pygame.display.update()
         fpsClock.tick(FPS)
         
