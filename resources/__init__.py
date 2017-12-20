7a9e71007985640e0388ec286b9390a155f1a976
Firstname Lastname <email_address>
2017-12-20 13:21:08 +0100

    tmx map imported (working)

--- resources/__init__.py
+++ resources/__init__.py
@@ -3,4 +3,4 @@ FPS = 30 # frames per second setting
 WIN_WIDTH = 640 # in pixel
 WIN_HEIGHT = 480 # in pixel
 SCREEN_SIZE = (WIN_WIDTH, WIN_HEIGHT)
-MAP = r'./map/map01.tmx'
+MAP = r'/home/umberto/Documents/python/pygame/game03/resources/map/map01.tmx'
