a04b3a68b6d2dfefc28d037aca8da1a806b86fce
Umberto Minora <minorau@oneteam.local>
2018-01-19 17:57:59 +0100

    use of relative path

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,4 +1,5 @@
 import pygame
+import os.path
 
 TITLE = '''Game 03'''
 FPS = 30 # frames per second setting
@@ -6,4 +7,4 @@ WIN_WIDTH = 640 # in pixel
 WIN_HEIGHT = 480 # in pixel
 SCREEN_SIZE = (WIN_WIDTH, WIN_HEIGHT)
 DISPLAYSURF = pygame.display.set_mode(SCREEN_SIZE)
-MAP = r'/home/umberto/Documents/python/pygame/game03/resources/map/map01.tmx'
+MAP = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'map', 'map01.tmx')
